c15cb67197498c3740278b2007c9e71082528fa9
jiaying2508 <38696658+jiaying2508@users.noreply.github.com>
2024-11-04 13:30:04 -0500

    Create pictographPlus.R

---
+++ R/pictographPlus.R
@@ -0,0 +1,70 @@
+#' @export
+#' 
+runPICTographPlus <- function(
+    mutation_file,
+    copy_number_file=NULL,
+    SNV_file=NULL,
+    rna_file=NULL,
+    outputDir=NULL,
+    sample_presence=TRUE,
+    dual_model=TRUE, # placeholder; dual_model=FALSE still require testing
+    score="silhouette", # either BIC or silhouette
+    ploidy=2, # placeholder
+    pval=0.05, # placeholder
+    max_K = 10, 
+    min_mutation_per_cluster=5, 
+    cluster_diff_thresh=0.05,
+    n.iter=5000, 
+    n.burn=1000, 
+    thin=10, 
+    mc.cores=8, 
+    inits=list(".RNG.name" = "base::Wichmann-Hill",".RNG.seed" = 123),
+    threshes=NULL,
+    LOH = FALSE,
+    alt_reads_thresh = 0, # placeholder
+    vaf_thresh = 0, # placeholder
+    cnv_max_dist=2000, # placeholder
+    cnv_max_percent=0.30, # placeholder
+    tcn_normal_range=c(1.7, 2.3), # placeholder
+    smooth_cnv=F, # placeholder
+    autosome=T # placeholder
+) {
+  
+  mcmcMain(mutation_file,
+           copy_number_file=copy_number_file,
+           SNV_file=SNV_file,
+           outputDir=outputDir,
+           sample_presence=sample_presence,
+           dual_model=dual_model, # placeholder; dual_model=FALSE still require testing
+           score=score, # either BIC or silhouette
+           ploidy=ploidy, # placeholder
+           pval=pval, # placeholder
+           max_K=max_K, 
+           min_mutation_per_cluster=min_mutation_per_cluster, 
+           cluster_diff_thresh=cluster_diff_thresh,
+           n.iter=n.iter, 
+           n.burn=n.burn, 
+           thin=thin, 
+           mc.cores=mc.cores, 
+           inits=inits,
+           threshes=threshes,
+           LOH=LOH,
+           alt_reads_thresh = alt_reads_thresh, # placeholder
+           vaf_thresh = vaf_thresh, # placeholder
+           cnv_max_dist=cnv_max_dist, # placeholder
+           cnv_max_percent=cnv_max_percent, # placeholder
+           tcn_normal_range=tcn_normal_range, # placeholder
+           smooth_cnv=smooth_cnv, # placeholder
+           autosome=autosome # placeholder
+  )
+  
+  treeFile = paste(outputDir, "tree.csv", sep="/")
+  proportionFile = paste(outputDir, "subclone_proportion.csv", sep="/")
+  purityFile = paste(outputDir, "purity.csv", sep="/")
+  
+  deconvolution(rna_file = rna_file,
+                treeFile = treeFile,
+                proportionFile = proportionFile,
+                purityFile = purityFile,
+                outputDir = outputDir)
+}
