af694b9c68331a892820c76c4f9f3ef6a42e67d7
jiaying2508 <38696658+jiaying2508@users.noreply.github.com>
2024-12-03 14:49:52 -0500

    Update deconvolution.R

--- R/deconvolution.R
+++ R/deconvolution.R
@@ -106,72 +106,76 @@ runGSEA <- function(X_optimal,
   
   gene_list <- read_GSEA_file(GSEA_file)
   
-  # run ssGSEA for each clone
-  gsvaPar <- ssgseaParam(X, gene_list)
-  gsva.es <- gsva(gsvaPar)
-  
-  # permutation test to generate p values
-  perm_results <- matrix(0, nrow = nrow(gsva.es), ncol = ncol(gsva.es))
-  # n_permutations <- 10000
-  
-  for (i in 1:n_permutations) {
-    # Permute gene labels
-    permuted_data <- X[sample(nrow(X)),,drop=FALSE]
-    rownames(permuted_data) <- rownames(X)
-    
-    # Perform ssGSEA on permuted data
-    permPar <- ssgseaParam(permuted_data, gene_list)
-    perm_ssgsea <- suppressMessages(gsva(permPar))
-    
-    # Store results
-    perm_results <- perm_results + ((gsva.es > 0 & gsva.es <= perm_ssgsea) | (gsva.es < 0 & gsva.es >= perm_ssgsea))
-  }
-  
-  p_values <- perm_results / n_permutations
-
-  p_values_corrected <- matrix(p.adjust(as.vector(p_values), method = "fdr"),
-                               nrow = nrow(p_values), 
-                               ncol = ncol(p_values))
-  colnames(p_values_corrected) = colnames(p_values)
-  rownames(p_values_corrected) = rownames(p_values)
+  # # run ssGSEA for each clone
+  # gsvaPar <- ssgseaParam(X, gene_list)
+  # gsva.es <- gsva(gsvaPar)
+  # 
+  # # permutation test to generate p values
+  # perm_results <- matrix(0, nrow = nrow(gsva.es), ncol = ncol(gsva.es))
+  # # n_permutations <- 10000
+  # 
+  # for (i in 1:n_permutations) {
+  #   # Permute gene labels
+  #   permuted_data <- X[sample(nrow(X)),,drop=FALSE]
+  #   rownames(permuted_data) <- rownames(X)
+  #   
+  #   # Perform ssGSEA on permuted data
+  #   permPar <- ssgseaParam(permuted_data, gene_list)
+  #   perm_ssgsea <- suppressMessages(gsva(permPar))
+  #   
+  #   # Store results
+  #   perm_results <- perm_results + ((gsva.es > 0 & gsva.es <= perm_ssgsea) | (gsva.es < 0 & gsva.es >= perm_ssgsea))
+  # }
+  # 
+  # p_values <- perm_results / n_permutations
+  # 
+  # p_values_corrected <- matrix(p.adjust(as.vector(p_values), method = "fdr"),
+  #                              nrow = nrow(p_values), 
+  #                              ncol = ncol(p_values))
+  # colnames(p_values_corrected) = colnames(p_values)
+  # rownames(p_values_corrected) = rownames(p_values)
   
   # plotting for clone level GSEA
-  samples <- colnames(gsva.es)
+  samples <- colnames(X)
   
   plots <- lapply(samples, function(sample_name) {
-    data_full <- prepare_barplot_data(gsva.es, p_values_corrected, sample_name)
     
-    write.csv(data_full, file = paste0(GSEA_dir,"/clone", sample_name, "_ssGSEA.csv"))
-    
-    data_top <- filter_top_pathways(data_full, n=top_K)
-    
-    # Top pathways barplot
-    horizontal_top_plot <- ggplot(data_top, aes(x = Log10Pval, y = reorder(Pathway, Enrichment), fill = Enrichment)) +
-      geom_bar(stat = "identity") +
-      geom_vline(xintercept = -log10(0.05), color = "green", linetype = "dashed", size = 1) +
-      scale_fill_gradientn(colors = c("blue", "red"), name = "NES", oob = scales::squish) +
-      labs(x = "-log10(p-adj)", y = "Pathway", title = paste("Top Enrichment Pathways for Clone ", sample_name)) +
-      theme(axis.text.y = element_text(size = 16), 
-            axis.text.x = element_text(size = 14),
-            axis.title.x = element_text(size = 18),           # X-axis title size
-            axis.title.y = element_text(size = 18),           # Y-axis title size
-            plot.title = element_text(size = 20) # Title size and style
-      )
-    
-    # Save the horizontal barplot for top pathways to a file
-    output_file_top <- paste0(GSEA_dir,"/clone", sample_name, "_ssGSEA_top", ".png") # Replace with desired file name
-    ggsave(output_file_top, plot = horizontal_top_plot, width = 13, height = 12)
-    
+    sample_gsea <- ssGSEA(X, sample_name, gene_list, GSEA_dir, n_permutations=n_permutations, n=top_K)
+    # 
+    # data_full <- prepare_barplot_data(gsva.es, p_values_corrected, sample_name)
+    # 
+    # write.csv(data_full, file = paste0(GSEA_dir,"/clone", sample_name, "_ssGSEA.csv"))
+    # 
+    # data_top <- filter_top_pathways(data_full, n=top_K)
+    # 
+    # # Top pathways barplot
+    # horizontal_top_plot <- ggplot(data_top, aes(x = Log10Pval, y = reorder(Pathway, Enrichment), fill = Enrichment)) +
+    #   geom_bar(stat = "identity") +
+    #   geom_vline(xintercept = -log10(0.05), color = "green", linetype = "dashed", size = 1) +
+    #   scale_fill_gradientn(colors = c("blue", "red"), name = "NES", oob = scales::squish) +
+    #   labs(x = "-log10(p-adj)", y = "Pathway", title = paste("Top Enrichment Pathways for Clone ", sample_name)) +
+    #   theme(axis.text.y = element_text(size = 16), 
+    #         axis.text.x = element_text(size = 14),
+    #         axis.title.x = element_text(size = 18),           # X-axis title size
+    #         axis.title.y = element_text(size = 18),           # Y-axis title size
+    #         plot.title = element_text(size = 20) # Title size and style
+    #   )
+    # 
+    # # Save the horizontal barplot for top pathways to a file
+    # output_file_top <- paste0(GSEA_dir,"/clone", sample_name, "_ssGSEA_top", ".png") # Replace with desired file name
+    # ggsave(output_file_top, plot = horizontal_top_plot, width = 13, height = 12)
+    # 
     # volcano plot
-    volcano_plot <- volcanoplot(data_full)
+    
   })
   
   edge_list <- read_tree(treeFile)
-  edge_list <- apply(edge_list, 2, as.character)
   
-  # gsea_results_list <- list()
+  gsea_results_list <- list()
   
   if (is.null(ncol(edge_list))) {
+    
+    edge_list <- as.character(edge_list)
     
     sample1 = edge_list[1]
     sample2 = edge_list[2]
@@ -180,6 +184,8 @@ runGSEA <- function(X_optimal,
     # gsea_results <- GSEA_diff(X, sample1, sample2, gene_list, n_permutations)
     
   } else {
+    edge_list <- apply(edge_list, 2, as.character)
+    
     for (i in 1:nrow(edge_list)) {
       
       sample1 = edge_list[i, 1]
@@ -195,21 +201,64 @@ runGSEA <- function(X_optimal,
   
 }
 
+ssGSEA <- function(X, sample_name, gene_list, GSEA_dir, n_permutations=10000, n=5) {
+  sample_ranked_genes <- sort(X[,sample_name], decreasing = T)
+  
+  # sample_gsea <- fgsea(pathways = gene_list, stats = sample_ranked_genes, nperm = n_permutations)
+  sample_gsea <- fgseaMultilevel(pathways = gene_list, stats = sample_ranked_genes)
+  
+  
+  sample_gsea$Log10padj <- -log10(sample_gsea$padj)
+  
+  # top_up <- sample_gsea %>% arrange(desc(NES)) %>% slice_head(n = n)
+  # top_down <- sample_gsea %>% arrange(NES) %>% slice_head(n = n)
+  # sample_gsea_top <- bind_rows(top_up, top_down)
+  sample_gsea_top <- sample_gsea %>% filter(padj < 0.05)
+  
+  horizontal_top_plot <- ggplot(sample_gsea_top, aes(x = Log10padj, y = reorder(pathway, NES), fill = NES)) +
+    geom_bar(stat = "identity") +
+    geom_vline(xintercept = -log10(0.05), color = "green", linetype = "dashed", linewidth = 1) +
+    scale_fill_gradientn(colors = c("blue", "red"), name = "NES", oob = scales::squish) +
+    labs(x = "-log10(p-adj)", y = "Pathway", title = paste0("Top Enrichment Pathways for Clone ", sample_name)) +
+    theme(axis.text.y = element_text(size = 16), 
+          axis.text.x = element_text(size = 14),
+          axis.title.x = element_text(size = 18),           # X-axis title size
+          axis.title.y = element_text(size = 18),           # Y-axis title size
+          plot.title = element_text(size = 20) # Title size and style
+    )
+  
+  # Save the horizontal barplot for top pathways to a file
+  output_file_top <- paste0(GSEA_dir,"/clone", sample_name, "_ssGSEA_top", ".png") # Replace with desired file name
+  ggsave(output_file_top, plot = horizontal_top_plot, width = 13, height = 12)
+  
+  volcano_plot <- volcanoplot(sample_gsea, paste0("Volcano plot for Clone ", sample_name), n=n)
+  
+  output_file_top <- paste0(GSEA_dir,"/clone", sample_name, "_ssGSEA_top_volcano", ".png") # Replace with desired file name
+  ggsave(output_file_top, plot = volcano_plot, width = 13, height = 12)
+  
+  leadingEdge <- sample_gsea$leadingEdge
+  sample_gsea$leadingEdge <- NULL
+  write.csv(as.data.frame(sample_gsea), file = paste0(GSEA_dir,"/clone", sample_name, "_ssGSEA.csv"))
+  sample_gsea$leadingEdge <- leadingEdge
+  return(sample_gsea)
+}
 
 GSEA_diff <- function(expr_matrix, sample1, sample2, gene_list, GSEA_dir, n_permutations=10000, n=5) {
   log2_diff <- log2((expr_matrix[, sample2] + 1) / (expr_matrix[, sample1] + 1))
   
   ranked_genes <- sort(log2_diff, decreasing = TRUE)
   
-  gsea_results <- fgsea(pathways = gene_list,
-                        stats = ranked_genes,
-                        nperm = n_permutations) 
-  
+  # gsea_results <- fgsea(pathways = gene_list,
+  #                       stats = ranked_genes,
+  #                       nperm = n_permutations)
+  gsea_results <- fgseaMultilevel(pathways = gene_list,stats = ranked_genes)
   gsea_results$Log10padj <- -log10(gsea_results$padj)
   
-  top_up <- gsea_results %>% arrange(desc(NES)) %>% slice_head(n = n)
-  top_down <- gsea_results %>% arrange(NES) %>% slice_head(n = n)
-  gsea_top <- bind_rows(top_up, top_down)
+  # top_up <- gsea_results %>% arrange(desc(NES)) %>% slice_head(n = n)
+  # top_down <- gsea_results %>% arrange(NES) %>% slice_head(n = n)
+  # gsea_top <- bind_rows(top_up, top_down)
+  # gsea_top <- gsea_top %>% filter(padj < 0.05)
+  gsea_top <- gsea_results %>% filter(padj < 0.05)
   
   horizontal_top_plot <- ggplot(gsea_top, aes(x = Log10padj, y = reorder(pathway, NES), fill = NES)) +
     geom_bar(stat = "identity") +
@@ -231,6 +280,11 @@ GSEA_diff <- function(expr_matrix, sample1, sample2, gene_list, GSEA_dir, n_perm
   gsea_results$leadingEdge <- NULL
   write.csv(as.data.frame(gsea_results), file = paste0(GSEA_dir,"/clone", sample2, "_", sample1, "_GSEA_diff.csv"))
   gsea_results$leadingEdge <- leadingEdge
+  
+  volcano_plot <- volcanoplot(gsea_results, paste0("Volcano plot for Top Differential Pathways between Clone ", sample2, " and Clone ", sample1), n=n)
+
+  output_file_top <- paste0(GSEA_dir,"/clone", sample2, "_", sample1, "_GSEA_diff_volcano", ".png") # Replace with desired file name
+  ggsave(output_file_top, plot = volcano_plot, width = 13, height = 12)
   
   return(gsea_results)
 }
@@ -263,33 +317,33 @@ read_tree <- function(treeFile) {
   return(edge_list)
 }
 
-volcanoplot <- function(data_full) {
+volcanoplot <- function(data_full, title, n=5) {
   data_full <- data_full %>%
-    mutate(Significance = ifelse(10^(-Log10Pval) < 0.05, 
-                                 ifelse(Enrichment < 0, "Negative", "Positive"), "Not Significant"))
+    mutate(Significance = ifelse(padj < 0.05, 
+                                 ifelse(NES < 0, "Negative", "Positive"), "Not Significant"))
   
   labels <- data_full %>%
-    arrange(desc(Enrichment)) %>%
-    slice_head(n = 5) %>%
+    arrange(desc(NES)) %>%
+    slice_head(n = n) %>%
     bind_rows(
       data_full %>%
-        arrange(Enrichment) %>%
-        slice_head(n = 5)
+        arrange(NES) %>%
+        slice_head(n = n)
     )
   
-  volcano_plot <- ggplot(data_full, aes(x = Enrichment, y = Log10Pval, color = Significance)) +
+  volcano_plot <- ggplot(data_full, aes(x = NES, y = Log10padj, color = Significance)) +
     geom_point(size = 3, alpha = 0.8) + # Add points with size and transparency
     scale_color_manual(values = c("Negative" = "blue", "Positive" = "red", "Not Significant" = "gray")) + # Custom colors
     # geom_text(data = labels, aes(label = Pathway), vjust = -1, size = 3.5) + # Add labels
-    geom_text_repel(data = labels, aes(label = Pathway), size = 3.5, max.overlaps = Inf) + # Non-overlapping labels
+    geom_text_repel(data = labels, aes(label = pathway), size = 3.5, max.overlaps = Inf) + # Non-overlapping labels
     labs(
-      title = "Volcano Plot",
+      title = title,
       x = "Enrichment Score",
       y = "-log10(P-value)",
       color = "Significant (p < 0.05)"
     ) +
-    xlim(-1, 1) + # Set x-axis limits
-    ylim(0, 3.2) +  # Set y-axis limits
+    # xlim(-1, 1) + # Set x-axis limits
+    # ylim(0, 3.2) +  # Set y-axis limits
     theme_minimal() +
     theme(legend.position = "top")
   
