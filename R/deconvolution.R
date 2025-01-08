a7b1a4fbd48cd1abe34d6d363b718b96df8cfcf0
jiaying2508 <38696658+jiaying2508@users.noreply.github.com>
2025-01-08 12:55:23 -0500

    update

--- R/deconvolution.R
+++ R/deconvolution.R
@@ -82,7 +82,7 @@ runDeconvolution <- function(rna_file,
 
 
 #' @export
-#' @import GSVA, pheatmap, limma, ggplot2, ggrepel, fgsea, GSEABase
+#' @import GSVA pheatmap limma ggplot2 ggrepel fgsea GSEABase
 runGSEA <- function(X_optimal,
                     outputDir,
                     treeFile,
